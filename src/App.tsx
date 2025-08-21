1cf4a663facf4f1efe501f6449d7f5949c1a9431
Hai Dang Nguyen <haidangnguyenus@gmail.com>
2025-08-21 15:43:38 -0700

    feat: render sample track list in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { SAMPLE_TRACKS } from "./features/tracks/data.sample.ts";
+import type { Track } from "./features/tracks/model.ts";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <section className="tracklist-panel">
+        {SAMPLE_TRACKS.map((track: Track) => (
+          <li key={track.id}>
+            {track.title} - {track.artist}
+          </li>
+        ))}
+      </section>
+      <section className="suggestions-panel"></section>
+      <section className="setlist-panel"></section>
+    </main>
+  );
 }
 
-export default App
+export default App;
