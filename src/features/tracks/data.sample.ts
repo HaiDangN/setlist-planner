74b72d38f1559b9cb63879675deab79478c93eb2
Hai Dang Nguyen <haidangnguyenus@gmail.com>
2025-08-21 14:40:43 -0700

    feat: create mock data

---
+++ src/features/tracks/data.sample.ts
@@ -0,0 +1,40 @@
+import type { Track } from "./model";
+
+export const SAMPLE_TRACKS: Track[] = [
+  {
+    id: "1",
+    title: "Divinity",
+    artist: "Porter Robinson",
+    bpm: 90,
+    genre: "Electronic",
+    energy: 3,
+    camelotKey: "10B",
+  },
+  {
+    id: "2",
+    title: "Latch",
+    artist: "Disclosure",
+    bpm: 122,
+    genre: "House",
+    energy: 2,
+    camelotKey: "3B",
+  },
+  {
+    id: "3",
+    title: "RATATA",
+    artist: "Skrillex",
+    bpm: 124,
+    genre: "House",
+    energy: 4,
+    camelotKey: "11B",
+  },
+  {
+    id: "3",
+    title: "One Chance",
+    artist: "Knock2",
+    bpm: 95,
+    genre: "House",
+    energy: 4,
+    camelotKey: "9B",
+  },
+];
